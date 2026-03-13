8314171af94220d334e930dab98f292954aeaaf6
Maja Saponjic <ms20210338@student.fon.bg.ac.rs>
2026-03-13 13:34:00 +0100

    Dodat movie-details.page

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // <-- dodaj
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router'; // <-- dodaj za eventualni routerLink
+import { RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../services/movie.service';
+import { RatingService, Rating } from '../../services/rating.service';
 import { Movie } from '../../services/movie.model';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   standalone: true,
-  imports: [
-    CommonModule,   // <-- dodaj
-    IonicModule,
-    RouterModule    // <-- ako koristiš routerLink
-  ],
+  imports: [CommonModule, IonicModule, RouterModule],
 })
 export class MovieDetailsPage implements OnInit {
-
   movie?: Movie;
+  ratings: Rating[] = [];
+  averageRating: number = 0;
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: MovieService
+    private movieService: MovieService,
+    private ratingService: RatingService
   ) {}
 
   ngOnInit() {
@@ -32,6 +31,19 @@ export class MovieDetailsPage implements OnInit {
         this.movie = data;
         console.log('Movie details:', this.movie);
       });
+
+      // Dohvati ocene za film
+      this.loadRatings(id);
+    }
+  }
+
+  async loadRatings(movieId: string) {
+    this.ratings = await this.ratingService.getRatingsForMovie(movieId);
+    if (this.ratings.length > 0) {
+      const sum = this.ratings.reduce((acc, r) => acc + r.rating, 0);
+      this.averageRating = sum / this.ratings.length;
+    } else {
+      this.averageRating = 0;
     }
   }
 }
